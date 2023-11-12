9c3eacdc46a52f082b23c380a8b44608ce3f64d4
kentbraaten <kentbraaten@frontiernet.net>
2023-11-11 18:09:31 -0600

    Stubbed in SlewRteLimiter to the drivetrain.

--- src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
@@ -7,6 +8,7 @@ import frc.robot.subsystems.DriveTrain;
 public class DefaultDriveCommand extends CommandBase {
     public XboxController xboxcontroller;
     public DriveTrain driveTrain;
+    private SlewRateLimiter limiter;
     
     
     public DefaultDriveCommand(DriveTrain dtrain, XboxController controller) {
@@ -14,10 +16,12 @@ public class DefaultDriveCommand extends CommandBase {
         this.addRequirements(driveTrain);
         dtrain.setDefaultCommand(this);
         xboxcontroller = controller;
+        limiter = new SlewRateLimiter(1, -1, 0);
     }
 
     @Override
     public void execute() {
+        //ToDo: use the limmiter to limit to calculate the speed.
         driveTrain.drive(xboxcontroller.getLeftX(),xboxcontroller.getLeftY()*-1);
         
     }
